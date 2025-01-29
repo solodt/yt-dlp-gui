4ebe94487374ebff7aef11ac116c4ca121284796
solodt <141768001+solodt@users.noreply.github.com>
2025-01-29 15:38:40 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
 #include <QSystemSemaphore>
 #include <QSharedMemory>
 #include <QMessageBox>
+#include <QFile>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QMessageBox error;
+    error.setIcon(QMessageBox::Critical);
     QSystemSemaphore semaphore("<sdtgui>", 1);
     semaphore.acquire();
     QSharedMemory sharedMemory("<sdtgui2>");
@@ -22,12 +25,29 @@ int main(int argc, char *argv[])
     semaphore.release();
     if (active)
     {
-        QMessageBox error;
-        error.setIcon(QMessageBox::Critical);
         error.setText("Приложение уже запущено.");
         error.exec();
         return 1;
     }
+    else if(!QFile::exists("yt-dlp.exe") || (!QFile::exists("ffmpeg.exe") || (!QFile::exists("ffprobe.exe")))){
+        QString text="Отсутствуют:";
+        int i=1;
+        if(!QFile::exists("yt-dlp.exe")){
+            text+="\n"+QString::number(i)+". yt-dlp.exe   ";
+            i++;
+        }
+        if(!QFile::exists("ffmpeg.exe")){
+            text+="\n"+QString::number(i)+". ffmpeg.exe   ";
+            i++;
+        }
+        if(!QFile::exists("ffprobe.exe")){
+            text+="\n"+QString::number(i)+". ffprobe.exe   ";
+            i++;
+        }
+        error.setText(text);
+        error.exec();
+        return 1;
+}
     MainWindow w;
     w.show();
     return a.exec();
